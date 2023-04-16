e4a767d0a3b7f13a89f1759d80f52a7bbffae668
Timicasto <timicasto@gmail.com>
2023-04-17 00:21:42 +0800

    firmware template and PCB constrains

---
+++ Firmware/HeartRateMonitor/cmake-build-debug/CMakeFiles/HeartRateMonitor.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/startup/startup_stm32g030xx.s" "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/cmake-build-debug/CMakeFiles/HeartRateMonitor.elf.dir/startup/startup_stm32g030xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32G030xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Inc"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Inc"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Inc/Legacy"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/CMSIS/Device/ST/STM32G0xx/Include"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Src/main.c" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/main.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Src/stm32g0xx_hal_msp.c" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Src/stm32g0xx_it.c" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/stm32g0xx_it.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/stm32g0xx_it.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Src/syscalls.c" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Core/Src/system_stm32g0xx.c" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/system_stm32g0xx.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Core/Src/system_stm32g0xx.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj.d"
+  "/home/timicasto/Projects/Hardware/HeartRateMonitor/Firmware/HeartRateMonitor/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj" "gcc" "CMakeFiles/HeartRateMonitor.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
