a1bc515689ad02dc60cbbf4a7f2749eeed75ac88
Timicasto Index <timicasto@gmail.com>
2023-05-18 03:09:12 +0800

    Change BKLT to Open Drain Output

--- Firmware/HeartRateMonitor/Core/Src/gpio.c
+++ Firmware/HeartRateMonitor/Core/Src/gpio.c
@@ -73,8 +73,15 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = SCREEN_DC_Pin|SCREEN_RES_Pin|SCREEN_BLK_Pin|SCREEN_CS_Pin;
+  GPIO_InitStruct.Pin = SCREEN_DC_Pin|SCREEN_RES_Pin|SCREEN_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  /*Configure GPIO pins: BKLT */
+  GPIO_InitStruct.Pin = SCREEN_BLK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
