afcbef577b4c07db769229fb5384bbe0bd4feb6f
Timicasto Index <timicasto@gmail.com>
2023-05-26 03:57:41 +0800

    rewrite algorithm

--- Firmware/HeartRateMonitor/Core/Inc/beat.h
+++ Firmware/HeartRateMonitor/Core/Inc/beat.h
@@ -2,36 +2,25 @@
 #define HEARTRATEMONITOR_BEAT_H
 
 #include "stm32g0xx.h"
-class MaxDetector {
-public:
-	bool update(uint16_t sample, uint16_t t);
-	uint16_t getSample();
-	uint16_t getTime();
-	uint16_t getDeltaTime();
-private:
-	uint16_t max_s = 0;
-	uint16_t max_t = 0;
-	uint16_t s = 0;
-	uint16_t t = 0;
-	uint16_t last_max_s = 0;
-	uint16_t last_max_t = 0;
-};
 
 class Beat
 {
 public:
 	bool update(uint16_t sample);
 	uint8_t getRate();
-private:
-	MaxDetector detector[8];
+	void poll();
 	
+	//MaxDetector detector[8];
+	bool isValid = false;
+	uint16_t threshold = 65535;
 	uint16_t t = 0;
-	
+
 	uint16_t last_beat_s = 0;
 	uint16_t last_beat_t = 0;
 	uint16_t beat_s = 0;
 	uint16_t beat_t = 0;
-	
+	uint16_t last_s = 0;
+
 	uint16_t T = 0;
 };
 
